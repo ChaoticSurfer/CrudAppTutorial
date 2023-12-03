886ebddba5e2099da5245dee49abbe3779935ff1
Anri Kezeroti <AKezeroti@abacusnext.com>
2023-12-03 23:20:07 +0400

    Person and PersonDbCOntext added

---
+++ CrudAppTutorial/PersonDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CrudAppTutorial
+{
+    public class PersonDbContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = dataabse.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
